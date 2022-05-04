042ef184e9f8a1a06282f8e481794b7aa240d200
Otavio Faria <oaugustofaria@gmail.com>
2022-05-04 13:35:18 -0300

    Subindo o projeto

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MeuTodo.Models;
+namespace MeuTodo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(connectionString: "DataSource=app.db;Cache=Shared");
+        }
+    }
+}
